cadf54e68ac180620890260ea60111d227f6b10e
asishkr123 <asishkrramuk@gmail.com>
2019-03-06 07:27:27 +0530

    Updates

---
+++ precache-manifest.ffc299c0f0ea42b09cab7d1a6bf6e3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bdf4c732738a416a815",
+    "url": "/upgrad/static/js/runtime~main.5bdf4c73.js"
+  },
+  {
+    "revision": "9b3b0c1950bb2ebd2f3d",
+    "url": "/upgrad/static/js/main.9b3b0c19.chunk.js"
+  },
+  {
+    "revision": "b6306bdf11b2c4c13c11",
+    "url": "/upgrad/static/js/2.b6306bdf.chunk.js"
+  },
+  {
+    "revision": "9b3b0c1950bb2ebd2f3d",
+    "url": "/upgrad/static/css/main.6135b665.chunk.css"
+  },
+  {
+    "revision": "8551019a2696fcaaf0d5348cea30ed4a",
+    "url": "/upgrad/index.html"
+  }
+];
